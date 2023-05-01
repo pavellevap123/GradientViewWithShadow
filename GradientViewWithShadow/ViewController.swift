3bd5ab00b35fe4abf6e8f97122e6dae688e74619
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2023-05-01 11:15:09 +0200

    Ready gradient view

--- GradientViewWithShadow/ViewController.swift
+++ GradientViewWithShadow/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private struct Constants {
+        static let viewHeightWidth: CGFloat = 120
+    }
+    
+    private lazy var gradientView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        let gradientLayer = setGradientBackground(topColor: .blue, bottomColor: .red)
+        gradientLayer.cornerRadius = 12
+        gradientLayer.shadowColor = UIColor.black.cgColor
+        gradientLayer.shadowOpacity = 1
+        gradientLayer.shadowOffset = .zero
+        gradientLayer.shadowRadius = 10
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: Constants.viewHeightWidth, height: Constants.viewHeightWidth)
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(gradientView)
+        layoutView()
     }
-
-
+    
+    private func setGradientBackground(topColor: UIColor, bottomColor: UIColor) -> CAGradientLayer {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = .zero
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        return gradientLayer
+    }
+    
+    private func layoutView() {
+        NSLayoutConstraint.activate([
+            gradientView.heightAnchor.constraint(equalToConstant: Constants.viewHeightWidth),
+            gradientView.widthAnchor.constraint(equalTo: gradientView.heightAnchor),
+            gradientView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            gradientView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100)
+        ])
+    }
 }
 
